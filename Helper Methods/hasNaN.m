e957b53fd0eef822f7d754e31b4557ccf4db4d0d
JonKing93 <jonking93@gmail.com>
2017-04-29 15:58:04 -0700

    Reduced "hasNaN" function to a single line of code.

--- Helper Methods/hasNaN.m
+++ Helper Methods/hasNaN.m
@@ -1,17 +1,4 @@
 function[NaNstatus] = hasNaN(Data)
 %% Checks if an N dimensional dataset contains any NaNs. Returns a boolean true if any NaNs are found.
 
-datadim = ndims(Data);
-
-reduction = Data;
-for k = 1:datadim
-    reduction = any(isnan(reduction));
-end
-
-if reduction == 1
-    NaNstatus = true;
-elseif reduction == 0
-    NaNstatus = false;
-else
-    error('NaNcheck not functioning correctly');
-end
+NaNstatus = any( isnan(Data(:)) );
